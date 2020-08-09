134e138d166b55bbabc7e0ce981471dcecceb315
Roman Evseev <useless47@gmail.com>
2020-08-09 19:40:43 +0300

    + JobLauncher

--- SpringBootBatchXmlToDb/src/test/java/com/revseev/batch/SpringBootBatchXmlToDbApplicationTests.java
+++ SpringBootBatchXmlToDb/src/test/java/com/revseev/batch/SpringBootBatchXmlToDbApplicationTests.java
@@ -2,6 +2,15 @@ package com.revseev.batch;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +18,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootBatchXmlToDbApplicationTests {
 
+	@Autowired
+	JobLauncher jobLauncher;
+	@Autowired
+	Job job;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void shouldRunJob() throws
+												  JobParametersInvalidException,
+												  JobExecutionAlreadyRunningException,
+												  JobRestartException,
+												  JobInstanceAlreadyCompleteException {
+		jobLauncher.run(job, new JobParameters());
+	}
 }

91ff08540c6c33a5ff4f516aac9133e554ccc2e2
Roman Evseev <useless47@gmail.com>
2020-08-13 22:54:38 +0300

    init - works

--- SpringBootBatchXmlToDb/src/main/java/com/revseev/batch/config/ParsingResourceProvider.java
+++ SpringBootBatchXmlToDb/src/main/java/com/revseev/batch/config/ParsingResourceProvider.java
@@ -1,6 +1,7 @@
 package com.revseev.batch.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
@@ -10,9 +11,12 @@ public class ParsingResourceProvider {
 
     @Value("${parsing.resource.path}")
     private String path;
+    @Value("${parsing.resource.classpath}")
+    private String classpath;
 
     public Resource provide() {
-        return new FileSystemResource(path);
+//        return new FileSystemResource(path);
+        return new ClassPathResource(classpath);
     }
 
 }
--- SpringBootBatchXmlToDb/src/main/resources/schema.sql
+++ SpringBootBatchXmlToDb/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS personType;
+DROP TABLE IF EXISTS person;
 
-CREATE TABLE personType  (
+CREATE TABLE person  (
     person_id BIGINT NOT NULL PRIMARY KEY,
     first_name VARCHAR(40),
     last_name VARCHAR(40),
